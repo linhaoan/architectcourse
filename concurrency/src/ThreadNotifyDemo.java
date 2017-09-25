82f921c5029b82e0966968a23a3088fdc1e7b9cd
linhaoan <330966654@qq.com>
2017-09-25 16:11:54 +0800

    wait and notify

---
+++ concurrency/src/ThreadNotifyDemo.java
@@ -0,0 +1,36 @@
+public class ThreadNotifyDemo {
+
+
+    public static void main(String[] args) {
+
+        final Object object = new Object();
+
+        Thread t1 = new Thread() {
+            public void run() {
+                synchronized (object) {
+                    System.out.println("T1 start!");
+                    try {
+                        System.out.println("T1 wait for object");
+                        object.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("T1 end!");
+                }
+            }
+        };
+        Thread t2 = new Thread() {
+            public void run() {
+                synchronized (object) {
+                    System.out.println("T2 start!");
+                    object.notify();
+                    System.out.println("T2 end!");
+                }
+            }
+        };
+        //如果t2先启动，会导致t1挂起
+        //TODO t2->t1 bug
+        t1.start();
+        t2.start();
+    }
+}
