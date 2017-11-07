8b2ea4d85329ff17518d8ecac62a3a68ca39024f
linhaoan <330966654@qq.com>
2017-11-07 22:27:03 +0800

    堆溢出和栈溢出

---
+++ jvm/JavaVMStackOOM.java
@@ -0,0 +1,32 @@
+package jvm;
+/**
+ * -Xss2M
+ *
+ */
+public class JavaVMStackOOM {
+	
+	private void doStop() {
+		while (true) {
+			
+		}
+	}
+	
+	public void stackLeakByThread() {
+		while (true) {
+			Thread thread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+					doStop();
+					
+				}
+				
+			});
+			thread.start();
+		}
+	}
+	public static void main(String[] args) {
+		JavaVMStackOOM oom = new JavaVMStackOOM();
+		oom.stackLeakByThread();
+	}
+}
---
+++ jvm/JavaVMStackSOF.java
@@ -0,0 +1,24 @@
+package jvm;
+
+/**
+ * -Xss:128k
+ *
+ */
+public class JavaVMStackSOF {
+	
+	private int stackLength = 1;
+
+	public void stackLeak() {
+		stackLength++;
+		stackLeak();
+	}
+	public static void main(String[] args) {
+		JavaVMStackSOF oom = new JavaVMStackSOF();
+		try {
+			oom.stackLeak();
+		} catch(Exception e) {
+			System.out.println( "stack length:" +oom.stackLength);
+			throw e;
+		}
+	}
+}
